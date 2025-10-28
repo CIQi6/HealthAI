73b6657750f040e9cc380e538cdb3ccd5496f2c2
CIQi6 <2153322382@qq.com>
2025-10-28 10:23:39 +0800

    chore: remove nested project directory

--- HealthAi/src/main/java/com/example/healthai/config/MyBatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.healthai.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan(basePackages = "com.example.healthai.**.repository")
-public class MyBatisConfig {
-}
